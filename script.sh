be9001fd36387996a4466d8a84de7c240a1d2101
Daniel George Holz <dgholz@gmail.com>
2026-04-06 10:33:29 +0100

    Use reviewdog action to install reviewdog

--- script.sh
+++ script.sh
@@ -1,13 +1,6 @@
 #!/bin/sh -e
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-TEMP_PATH="$(mktemp -d)"
-PATH="${TEMP_PATH}:$PATH"
-
-echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/reviewdog'
-curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" 2>&1
-echo '::endgroup::'
-
 if [ "${INPUT_USE_BUNDLER}" = "true" ]; then
   echo '::group:: Installing erb-lint via bundler'
   bundle install
